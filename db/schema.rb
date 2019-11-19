5706f9327a0793adce0674329cb228b11f3cd809
Ryan Hantak <rhantak@gmail.com>
2019-11-18 23:39:34 -0700

    Add merchant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_063837) do
+ActiveRecord::Schema.define(version: 2019_11_19_063920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2019_11_19_063837) do
     t.string "name"
     t.text "description"
     t.float "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
