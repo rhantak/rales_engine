1f6ee51d469b04313cd0ba6fe23adf0ef28df543
Ryan Hantak <rhantak@gmail.com>
2019-11-21 13:07:57 -0700

    Add class method for finding top merchants by revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,15 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def self.most_revenue(limit)
+    Merchant.select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue').
+    joins(:invoice_items, :transactions).
+    group(:id).
+    order('revenue desc').
+    limit(limit)
+    # merge(Transaction.successful).
+  end
 end
