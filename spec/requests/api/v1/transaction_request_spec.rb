8db3aff4689b8dcff7ce6c28ba1c0931837ddbfd
Ryan Hantak <rhantak@gmail.com>
2019-11-27 22:25:46 -0600

    Add test for transaction record endpoints

---
+++ spec/requests/api/v1/transaction_request_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe 'Transactions API' do
+  describe 'record endpoints' do
+    before(:each) do
+      @customer = create(:customer)
+      @invoice = create(:invoice, customer_id: @customer.id)
+    end
+
+    it "sends a list of transactions" do
+      create_list(:transaction, 3, invoice_id: @invoice.id)
+
+      get '/api/v1/transactions'
+
+      expect(response).to be_successful
+
+      transactions = JSON.parse(response.body)
+
+      expect(transactions["data"].count).to eq(3)
+    end
+
+    xit "can get one transaction by its id" do
+      id = create(:transaction, customer_id: @customer.id).id
+
+      get "/api/v1/transactions/#{id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(transaction["data"]["id"]).to eq(id.to_s)
+    end
+
+    xit "can find a transaction by its attributes" do
+      transaction_1 = create(:transaction, result: 'failure', invoice_id: @invoice.id)
+      transaction_2 = create(:transaction, result: 'success', invoice_id: @invoice.id)
+
+      get '/api/v1/transactions/find?result=success'
+
+      expect(response).to be_successful
+
+      api_transaction = JSON.parse(response.body)
+
+      expect(transaction_1.id.to_s).to eq(api_transaction["data"]["id"])
+    end
+
+    xit "can find all items matching an attribute" do
+      transaction_1 = create(:transaction, result: 'failure', invoice_id: @invoice.id)
+      transaction_2 = create(:transaction, result: 'success', invoice_id: @invoice.id)
+      transaction_3 = create(:transaction, result: 'success', invoice_id: @invoice.id)
+
+      get '/api/v1/transaction/find_all?result=success'
+
+      expect(response).to be_successful
+
+      transactions = JSON.parse(response.body)
+
+      expect(transactions["data"].count).to eq(3)
+      expect(transactions["data"][1]["id"]).to eq(transaction_2.to.to_s)
+      expect(transactions["data"][2]["id"]).to eq(transaction_3.to.to_s)
+    end
+
+    xit "can send a random transaction" do
+      create_list(:transaction, 3, customer_id: @customer.id)
+
+      get '/api/v1/transactions/random'
+
+      expect(response).to be_successful
+
+      expect(response.body).to_not eq("")
+    end
+  end
+end
