18f4cf493ae6d2f9fe01b064f8e9e74fa17870b0
Ryan Hantak <rhantak@gmail.com>
2019-11-28 23:18:31 -0600

    Add method to return a single merchant for an invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,12 +4,17 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :transactions
   has_many :invoice_items
-  has_many :merchants
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
   def self.revenue_on_date(target)
     joins(:invoice_items, :transactions).
     where( transactions: {result: :success}).
     where( invoices: {created_at: target.to_date.all_day}).
     sum('quantity * unit_price')
+  end
+
+  def merchant
+    merchants.distinct[0]
   end
 end
