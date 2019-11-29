9613315023b37e62f351b5db6d402930c69a57ac
Ryan Hantak <rhantak@gmail.com>
2019-11-28 23:29:18 -0600

    Correct the relationship between invoice and merchant

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,19 +2,15 @@ class Invoice < ApplicationRecord
   validates_presence_of :status
 
   belongs_to :customer
+  belongs_to :merchant
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :merchants, through: :items
 
   def self.revenue_on_date(target)
     joins(:invoice_items, :transactions).
     where( transactions: {result: :success}).
     where( invoices: {created_at: target.to_date.all_day}).
     sum('quantity * unit_price')
-  end
-
-  def merchant
-    merchants.distinct[0]
   end
 end
