1c4833d9d794cf5fc51c63fb7220a7b75889c714
Ryan Hantak <rhantak@gmail.com>
2019-11-30 21:21:59 -0600

    Add method to find merchant a customer has the most transactions with

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,14 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :merchants, through: :invoices
+
+  def favorite_merchant
+    Customer.joins(:transactions, :merchants).
+    select('merchants.*, count(transactions) as purchases').
+    group('merchants.id, customers.id').
+    where(customers: {id: id}).
+    merge(Transaction.successful).
+    order('transactions.count desc').
+    first
+  end
 end
